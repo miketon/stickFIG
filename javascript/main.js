d12dcd2b2f49a6497f38682c17329c52d0ecf517
Mike Ton <mike.ton@gmail.com>
2011-10-04 23:26:00 -0700

    Adding globals to reduce passing complexity

--- javascript/main.js
+++ javascript/main.js
@@ -11,6 +11,12 @@ function initCanvas(width, height){
   return canvas                     ;
 }
 
+function clearCanvas(){
+  CTX.setTransform(1,0,0,1,0,0)           ; //reset canvas
+  CTX.fillStyle = 'rgba(122,122,122,1.0)' ;
+  CTX.fillRect(0, 0, C.w, C.h)            ;
+}
+
 function helloWorld(){
   alert('The world!');
 }
