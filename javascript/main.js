5f0d630e0690d371cb98354039577016ae0a0183
Mike Ton <mike.ton@gmail.com>
2011-10-11 00:48:04 -0700

    Wrapping up canvas init in library. Cleaning up globals.

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,31 @@
-function initCanvas(width, height){
-  $      = function(id)  { return document.getElementById(id) } ;
-  var dc = function(tag) { return document.createElement(tag) } ;
+(function(){
 
-  var canvas    = dc('canvas')      ;
-  canvas.id     = 'canvasMT'        ;
-  canvas.width  = width             ;
-  canvas.height = height            ;
-  document.body.appendChild(canvas) ;
+  var C = window.C = function(){
+    alert('Library Wrapping');
+  }
 
-  return canvas                     ;
-}
+  C.init = function(){
 
-function clearCanvas(){
-  CTX.setTransform(1,0,0,1,0,0)           ; //reset canvas
-  CTX.fillStyle = 'rgba(122,122,122,1.0)' ;
-  CTX.fillRect(0, 0, C.w, C.h)            ;
-}
+    var  $ = function(id)  { return document.getElementById(id) } ;
+    var dc = function(tag) { return document.createElement(tag) } ;
+    var w  = 320                                                  ;
+    var h  = 240                                                  ;
 
-function helloWorld(){
-  alert('The world!');
-}
+    var canvas    = dc('canvas')      ;
+    canvas.id     = 'canvasMT'        ;
+    canvas.width  = w                 ;
+    canvas.height = h                 ;
+    document.body.appendChild(canvas) ;
+
+    C.x   = canvas.offsetLeft       ; //delta from x.offsetParent (body)
+    C.y   = canvas.offsetTop        ; //alt = e.offsetX (will tile?)
+    var c = canvas.getContext('2d') ;
+
+    C.clearCanvas = function(){
+      c.setTransform(1,0,0,1,0,0)           ; //reset canvas
+      c.fillStyle = 'rgba(122,122,122,1.0)' ;
+      c.fillRect(0, 0, w, h)                ;
+    }()                                     ;
+  }
+
+})();
